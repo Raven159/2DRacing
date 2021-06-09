a33d76649a76904946de4a3ba85f4d254f825e45
Andrey <Raven159@yandex.ru>
2021-06-09 20:37:42 +0300

    Add scripts
    IReadOnlySubscriptionAction
    SubscriptionAction
    Set up the project

---
+++ 2DRacing/Assets/Scripts/IReadOnlySubscriptionAction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Tools
+{
+    internal interface IReadOnlySubscriptionAction
+    {
+        void SubscribeOnChange(Action subscriptionAtion);
+        void UnSubscriptionOnChange(Action unsubscriptionAction);
+    }
+}
