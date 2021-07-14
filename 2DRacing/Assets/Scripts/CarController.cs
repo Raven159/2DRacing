d5944e03406705ce4c06f483b378f27327cecb19
Andrey Zakharov <raven159@yandex.ru>
2021-07-14 23:45:31 +0300

    Lesson 3

---
+++ 2DRacing/Assets/Scripts/CarController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CarController : BaseController, IAbilityActivator
+{
+    public GameObject GetViewObject()
+    {
+        throw new NotImplementedException();
+    }
+}
+
